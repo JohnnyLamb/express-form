fccef5f30bfe1013945722b3679c91295d12727f
johnny lamb <agnusjack@gmail.com>
2015-08-20 16:34:16 -0600

    finished the form express

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/submit', function(req, res, next) {
+  res.redirect('/success');
+});
+
+router.get('/success', function(req, res, next) {
+  res.send('success!');
+});
+module.exports = router;
